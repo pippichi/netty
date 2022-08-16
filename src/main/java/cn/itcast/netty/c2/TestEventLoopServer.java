6a083549575879823e6fe05acf5af17b20d9676d
qyf <emoqyf@163.com>
2022-08-16 22:30:17 +0800

    netty第一颗课

---
+++ src/main/java/cn/itcast/netty/c2/TestEventLoopServer.java
@@ -0,0 +1,36 @@
+package cn.itcast.netty.c2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+@Slf4j
+public class TestEventLoopServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                //      boss 只负责ServerSocketChannel上的accept事件       worker只负责SocketChannel上的读写事件
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup(2))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override                                           // ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                            }
+                        });
+                    }
+                })
+                .bind(8888);
+    }
+}
