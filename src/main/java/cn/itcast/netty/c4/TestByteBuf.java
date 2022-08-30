95ffca9b5e6ca1a3b90f108e99cdcbb553d333b0
qyf <emoqyf@163.com>
2022-08-30 23:01:43 +0800

    EmbededChannel

---
+++ src/main/java/cn/itcast/netty/c4/TestByteBuf.java
@@ -0,0 +1,35 @@
+package cn.itcast.netty.c4;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+
+import static io.netty.buffer.ByteBufUtil.appendPrettyHexDump;
+import static io.netty.util.internal.StringUtil.NEWLINE;
+
+public class TestByteBuf {
+    public static void main(String[] args) {
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+        System.out.println(buffer);
+        ByteBuf heapBuffer = ByteBufAllocator.DEFAULT.heapBuffer();
+        System.out.println(heapBuffer);
+        log(buffer);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 32; ++i) {
+            sb.append("a");
+        }
+        buffer.writeBytes(sb.toString().getBytes());
+        log(buffer);
+    }
+
+    private static void log(ByteBuf buf) {
+        int length = buf.readableBytes();
+        int rows = length / 16 + (length % 15 == 0 ? 0 : 1) + 4;
+        StringBuilder sb = new StringBuilder(rows * 80 * 2)
+                .append("read index:").append(buf.readerIndex())
+                .append(" write index:").append(buf.writerIndex())
+                .append(" capacity:").append(buf.capacity())
+                .append(NEWLINE);
+        appendPrettyHexDump(sb, buf);
+        System.out.println(sb.toString());
+    }
+}
