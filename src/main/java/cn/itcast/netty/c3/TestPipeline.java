0e2adc35b8f7441dc5afbcddf0799775527f7fa1
qianyifeng <emoqyf@163.com>
2022-08-31 15:09:45 +0800

    TestPipeline

--- src/main/java/cn/itcast/netty/c3/TestPipeline.java
+++ src/main/java/cn/itcast/netty/c3/TestPipeline.java
@@ -1,4 +1,4 @@
-package cn.itcast.netty.c2.c3;
+package cn.itcast.netty.c3;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
@@ -37,19 +37,36 @@ public class TestPipeline {
                             public void channelRead(ChannelHandlerContext ctx, Object name) throws Exception {
                                 log.debug("2");
                                 Student student = new Student(name.toString());
-                                super.channelRead(ctx, student);
+                                super.channelRead(ctx, student); // 将数据传递给下一个handler，如果不调用，调用链就会断开
                             }
-                        }).addLast("h3", new ChannelInboundHandlerAdapter() {
+                        });
+
+                        pipeline.addLast("h7", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("7");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+
+                        pipeline.addLast("h3", new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                 log.debug("3，结果：{}，class：{}", msg, msg.getClass());
 //                                super.channelRead(ctx, msg); channelRead表示执行下一个入栈处理器 由于后续已经没有入栈处理器了所以这个方法可以不写了
-                                ch.writeAndFlush(ctx.alloc().buffer().writeBytes("server...".getBytes()));
+//                                ch.writeAndFlush(ctx.alloc().buffer().writeBytes("server...".getBytes()));
+                                ctx.writeAndFlush(ctx.alloc().buffer().writeBytes("server...".getBytes())); // 注意NioSocketChannel可以从tail往前找ChannelOutboundHandlerAdapter，ChannelHandlerContext是从当前handler往前找的。因此在这里ChannelHandlerContext只能找到h7而找不到h4、h5、h6
                             }
                         });
 
-                        // 添加出栈处理器（ChannelOutboundHandlerAdapter） head -> .. -> h5 -> h6 -> h7 -> tail 出栈处理器由tail往前执行
-                        pipeline.addLast("h5", new ChannelOutboundHandlerAdapter() {
+                        // 添加出栈处理器（ChannelOutboundHandlerAdapter） head -> .. -> h4 -> h5 -> h6 -> tail 出栈处理器由tail往前执行
+                        pipeline.addLast("h4", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("4");
+                                super.write(ctx, msg, promise);
+                            }
+                        }).addLast("h5", new ChannelOutboundHandlerAdapter() {
                             @Override
                             public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
                                 log.debug("5");
@@ -59,12 +76,6 @@ public class TestPipeline {
                             @Override
                             public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
                                 log.debug("6");
-                                super.write(ctx, msg, promise);
-                            }
-                        }).addLast("h7", new ChannelOutboundHandlerAdapter() {
-                            @Override
-                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-                                log.debug("7");
                                 super.write(ctx, msg, promise);
                             }
                         });
